9de283d8a84d99e3b9edb916aaa0c1a4bf7d80e1
whoKnowsss <1165527916@qq.com>
2017-05-31 22:13:24 +0800

    管道修改
    增加进程互斥

--- os/Ex4_linux进程管理/3_pipe/xf_pipe.c
+++ os/Ex4_linux进程管理/3_pipe/xf_pipe.c
@@ -24,30 +24,32 @@
 int fd[2]; //fd[0]:读管道，fd[1]:写管道
 //成功创建子进程，进行写操作
 void WritePipe(int id){
-        printf("Xf's child %d,start write!!\n",id+1);
+      
 		//子进程进行写入前先关闭读取管道
         close(fd[0]);
-        char str[25];
+        lockf(fd[1],1,0); //对写管道加锁
+        printf("Xf's child %d (pid=%d),start write!!\n",id+1,getpid());
+        char mesg[100];
         // itoa(id+1, str, 10);   int转字符串 linux没这个函数
-        sprintf(str, "%d", id+1);     // 使用sprintf将整数转换成字符串  
-        strcat(str," !!\n");    //拼接到最后
-	    char mesg[100] = "this message from xf's child ";
-        strcat(mesg,str);
+        // 使用sprintf将整数转换成字符串  
+        sprintf(mesg, "this message from xf's child %d (pid=%d) !!\n", id+1,getpid()); 
         //write函数写入失败时返回-1，成功时返回写入的字节数
         int re = (int)write(fd[1], mesg, size);
         if(  re != -1 ){
-            printf("子进程%d写入完毕,写入的字节数为%d\n",id+1,re);
+            printf("子进程%d(pid=%d)写入完毕,写入的字节数为%d\n",id+1,getpid(),re);
         }
         else{
-            printf("子程序%d写入失败\n",id);
+            printf("子程序%d(pid=%d)写入失败\n",id,getpid());
         }
-        
+        sleep(3);   //子进程沉睡3s,保证父进程可以从管道读出数据
+        lockf(fd[1],0,0);  //解锁
         exit(0);
 }
 int main()
 {
 	pid_t pid[3]; //3个子进程
 	char getMesg[40];
+	int i;
 	if(pipe(fd)<0)    //创建管道失败
 		{
 			//标准错误流，内核在处理stdout和stderr时优先级不一
@@ -99,14 +101,14 @@ int main()
 			//读取前关闭写入管道
             close(fd[1]);
     
-            printf("xf's父进程开始读取\n");
+            printf("xf's父进程%d开始读取：\n",getpid());
     
-             for (int i=0; i<3; i++) 
+             for ( i=0; i<3; i++) 
                {
                   if ( read(fd[0], getMesg, size) != -1 ) 
                      printf("%s\n",getMesg);
                   else
-                     printf("读取子进程%d的失败\n",i);              
+                     printf("读取子进程%d的失败！！\n",i);              
                }
 			printf("-------------Xf's End of Program-----------\n");
 			return 0;
